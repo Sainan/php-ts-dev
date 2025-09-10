e0e0f48e9aae208c45e465ce572b2ffca2b4d56c
Sainan <63328889+Sainan@users.noreply.github.com>
2025-09-10 08:46:11 +0200

    Refresh page after server restart (#3)

--- inject-script.js
+++ inject-script.js
@@ -1,6 +1,21 @@
 (() => {
+	const ping = () => {
+		fetch(`/php-ts-dev-ping?${Date.now()}`, { cache: "no-store" })
+			.then((res) => {
+				if (res.status < 500) {
+					location.reload();
+				} else {
+					setTimeout(ping, 1000);
+				}
+			})
+			.catch(() => {
+				setTimeout(ping, 1000);
+			});
+	};
+
 	const ws = new WebSocket("/php-ts-dev-ws");
 	ws.onmessage = () => {
 		location.reload();
 	};
+	ws.onclose = ping;
 })();
