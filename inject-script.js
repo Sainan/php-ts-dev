9d31987772b144a84c93d8b85f460e983d6012af
Sainan <63328889+Sainan@users.noreply.github.com>
2025-09-10 08:34:21 +0200

    Reload after successful ping response

--- inject-script.js
+++ inject-script.js
@@ -1,6 +1,21 @@
 (() => {
+	const startPing = () => {
+		const interval = setInterval(() => {
+			fetch(`/php-ts-dev-ping?${Date.now()}`, { cache: "no-store" })
+				.then((res) => {
+					if (res.status < 500) {
+						clearInterval(interval);
+						location.reload();
+					}
+				})
+				.catch(() => {});
+		}, 1000);
+	};
+
 	const ws = new WebSocket("/php-ts-dev-ws");
 	ws.onmessage = () => {
 		location.reload();
 	};
+	ws.onclose = startPing;
+	ws.onerror = startPing;
 })();
